2466efc573b0a8287974ea5034951c44f9f53d9f
Jade Blue <jayjeet.mandhata98@gmail.com>
2023-10-10 23:42:16 +0530

    mongo task 1

--- util/database.js
+++ util/database.js
@@ -1,8 +1,26 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+let _db;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (cb)=>{
+  MongoClient.connect('mongodb+srv://Jade:admin@cluster0.snf2pm9.mongodb.net/?retryWrites=true&w=majority')
+  .then(client=>{
+    console.log(`db connection succesful`)
+    _db = client.db()
+    cb(client)
+  })
+  .catch(e=>{
+    console.log(e)
+    throw e
+  })
+}
+const getDb= ()=>{
+  if(_db)
+  {
+    return _db
+  }
+  throw 'No db found'
+}
 
-module.exports = sequelize;
+exports.mongoConnect = mongoConnect
+exports.getDb = getDb
